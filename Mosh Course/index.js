51d11ead135a0fc25d168b7ac26c681f0068e534
Teoneag <teodor.neagoe.climber@gmail.com>
2023-05-16 12:17:17 +0300

    first-commit-step-0

---
+++ Mosh Course/index.js
@@ -0,0 +1,18 @@
+function createCircle(radius) {
+    return{
+        radius,
+        draw: function () {
+            console.log('draw');
+        }
+    };
+}
+
+const circle = createCircle(1);
+
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function(){
+        console.log('draw');
+    }
+}
+const another = new Circle(1);
